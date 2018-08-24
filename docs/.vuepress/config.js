b8c8298e52246220bf3f7643dd0d261fe69bda3b
Ladislav Sulc <ladislav.sulc@gmail.com>
2018-08-24 21:58:02 +0200

    First commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'Hi VuePress',
+    description: 'Just playing around',
+    head: [
+		['link', { rel: 'icon', href: `/favicon.png` }]
+	],
+	ga: '',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Test', link: '/test/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+    },
+    sidebar: 'auto'
+}
